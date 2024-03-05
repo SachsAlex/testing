70dd9f07fcd60ae83ce48ff5f3b2d86c8598bed6
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-03-05 14:52:07 +0100

    add_jsErrors; edit reference+type

---
+++ unit-testing/javascript-errors/referenceError.js
@@ -0,0 +1,9 @@
+// Folgender Code wirft einen ReferenceError
+
+try {
+  meineLieblingsFunktion();
+} catch (error) {
+  console.log("Mistakes were made");
+} finally {
+  console.log("Fix your code");
+}
